d47671503cb478425c5d76d06847b5f456588660
Mattia Gasparotto <gasmat04@gmail.com>
2025-07-25 11:15:11 +0200

    Project init

---
+++ svelte.config.js
@@ -0,0 +1,22 @@
+import adapter from '@sveltejs/adapter-netlify';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+export default {
+	kit: {
+		// for more information about preprocessors
+		preprocess: vitePreprocess(),
+
+		// default options are shown
+		adapter: adapter({
+			// if true, will create a Netlify Edge Function rather
+			// than using standard Node-based functions
+			edge: false,
+
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app.
+			// if `edge` is true, this option cannot be used
+			split: false
+		})
+	}
+};
